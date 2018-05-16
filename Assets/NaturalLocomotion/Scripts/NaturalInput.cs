73cbff8e7568710ed6a003059ee15a5579688449
Ronnie Smith <halfpint1170@gmail.com>
2018-05-15 21:57:42 -0700

    Quick comments at the end of my day

--- Assets/NaturalLocomotion/Scripts/NaturalInput.cs
+++ Assets/NaturalLocomotion/Scripts/NaturalInput.cs
@@ -2,8 +2,16 @@
 {
     using UnityEngine;
 
+    /// <summary>
+    /// Base class for input handlers that connect to Locomotion hub
+    /// and provide input vectors to move the player
+    /// </summary>
     public class NaturalInput : HubConnector
     {
+        /// <summary>
+        /// Gets the current input vector, will only be called if the input is active.
+        /// </summary>
+        /// <returns>Normalized input for the current frame</returns>
         public virtual Vector3 GetVector()
         {
             return Vector3.zero;
